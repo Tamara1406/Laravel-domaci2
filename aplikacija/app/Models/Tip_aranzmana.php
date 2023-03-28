8e2a725227ac43df28009800f28a48db2fc6494d
Tamara1406 <tamaram0053@gmail.com>
2023-03-28 12:49:16 +0200

    Kreirani modeli

---
+++ aplikacija/app/Models/Tip_aranzmana.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tip_aranzmana extends Model
+{
+    use HasFactory;
+}
